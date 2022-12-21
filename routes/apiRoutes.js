f00a4d73233c8d1a98aae4e931920118daa559e7
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-12-20 21:41:39 -0800

    Fixed issue where notes weren't populating, I had three instances of Res.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,33 @@
 const router = require("express").Router(); 
+// Sets up the handler for random ids' 
 const uuid = require('../InsertID/uuid');
+// loads the fs to write the files
 const fs = require('fs');
+
+
 router.get('/', (req,res)=> {
     // This gets the notes from the DB
-    res.json(`GOT YOUR ${req.method} REQUEST!!!`);
+    console.log(`GOT YOUR ${req.method} REQUEST!!!`);
+    const datafromJson = fs.readFileSync('./db/db.json', 'utf8');
+    res.json(JSON.parse(datafromJson)); 
 })
 
 router.post('/', (req,res) => {
     // This adds the notes to the DB 
-    res.json(`GOT YOUR ${req.method} REQUEST!!!`);
+    console.log(`GOT YOUR ${req.method} REQUEST!!!`);
     const {title, text} = req.body;
     if (title && text ) {
     const newNote = { 
         title, text, 
         id: uuid(), 
-    } 
+    };
     // Need to parse the notes that are currently being used or typed 
-    const currentNotes = JSON.parse(fs.readFileSync('.db/db.json', 'utf8'));
+    const currentNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     currentNotes.push(newNote); 
 
-    fs.writeFile('.db/db.json', JSON.stringify(currentNotes), (err) => err
+    fs.writeFile('./db/db.json', JSON.stringify(currentNotes), (err) => err
     ? console.log(err) 
-    : console.log(`Note for ${newNote.title} has been taken, please update your note!`)
+    : console.log(`Note for ${newNote.title} has been written down!`)
     );
     const response = { 
         status: 'Note has been succesfully saved!',
